b05ee6cf713d76cb596c4e357492ac3338a8167c
Kaue Bemfica <kauebemfica@hotmail.com>
2024-01-09 12:36:13 -0300

    ProjetoCelularesFinal

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone 
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(11) 991692205", "Tijolão", "00000000", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+
+Iphone iphone = new Iphone("(11) 991692205", "iPhone 15", "7777777", 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("facebook");
